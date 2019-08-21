52de58ccfebeb29fb6aea706235efceb240bfb60
Tommy <tommyzhangchina@gmail.com>
2019-08-21 14:05:31 +0800

    Week 4 lab 5 update

--- week 4/rep.c
+++ week 4/rep.c
@@ -4,9 +4,54 @@
 #include <ctype.h>
 
  void replace( char oldword[], char newword[], char whole_sentence[] ) {
-     int oo = strlen(oldword);
-     int ss = strlen(whole_sentence);
-     for 
+    int oo = strlen(oldword);
+    int ss = strlen(whole_sentence);
+    int nn = strlen(newword);
+    int match[ss];
+    int matchCount = 0;
+    for (int i = 0; i < ss; i++)
+    {
+        match[i]=0;
+    }
+    
+    for (int i = 0;i<(ss-oo+1);i++){
+        int isMatch = 1;
+        if(whole_sentence[i]==oldword[0]){
+            for (int j =1;j < oo; j++)
+            {
+                if(whole_sentence[i+j]!=oldword[j]) {
+                    isMatch = 0;
+                    break;
+                }
+            }
+            if (isMatch)
+            {
+                match[i]=1;
+                matchCount++;
+            }
+        }
+    }
+
+    char result[ss+matchCount*(nn-oo)+1];
+    int already = 0;
+    for (int i = 0; i < ss; i++)
+    {
+        if (match[i]==0)
+        {
+            result[i+already]=whole_sentence[i];
+        } else {
+            int j = 0;
+            for (; j < nn; j++)
+            {
+                result[i+j+already] = newword[j];
+            }
+            already+=j-1;
+            i+=nn-1;
+        }
+    }
+
+    printf("%s \n",result);
+    
  }
 
  int main(int argc, char *argv[])
