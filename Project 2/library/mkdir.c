3cd88d1526794c0dc072dfeb4b8206c0fe523386
Tommy <tommyzhangchina@gmail.com>
2019-10-07 23:17:51 +0800

    Project 2 1.5
    关于 各功能的分析 (3/7)

--- Project 2/library/mkdir.c
+++ Project 2/library/mkdir.c
@@ -1,8 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "sifs-internal.h"
+
+/* CITS2002 Project 2 2019
+   Name(s):             Tommy Zhang, Zhenyu Yang
+   Student number(s):   22181467 22374571
+ */
+
 
 // make a new directory within an existing volume
 int SIFS_mkdir(const char *volumename, const char *pathname)
 {
-    SIFS_errno	= SIFS_ENOTYET;
-    return 1;
+    //  ENSURE THAT RECEIVED PARAMETERS ARE VALID
+    if(volumename == NULL || pathname == NULL) {
+	SIFS_errno	= SIFS_EINVAL;
+	return 1;
+    }
+
+    //ATTEMPT TO OPEN THE VOLUME - OPEN FOR WRITING
+    FILE *vol	= fopen(volumename, "r+");
+
+    //ENSURE THAT THE FILE IS OPENED
+    if(vol = NULL) {
+	SIFS_errno	= SIFS_ENOTFILE;
+	return 1;
+    }
+
+//  FINISHED, CLOSE THE VOLUME
+    fclose(vol);
+
+//  AND RETURN INDICATING SUCCESS
+    return 0;
 }
