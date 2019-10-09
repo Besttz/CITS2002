1d2ae840f04511d1ec1452a591059ea64d83f8bf
Tommy <tommyzhangchina@gmail.com>
2019-10-09 13:53:38 +0800

    Project 2 2.9.1

--- Project 2/library/dirinfo.c
+++ Project 2/library/dirinfo.c
@@ -1,6 +1,5 @@
 #include "sifs-new.h"
 
-
 /* CITS2002 Project 2 2019
    Name(s):             Tommy Zhang, Zhenyu Yang
    Student number(s):   22181467 22374571
@@ -10,9 +9,9 @@
 int SIFS_dirinfo(const char *volumename, const char *pathname,
                  char ***entrynames, uint32_t *nentries, time_t *modtime)
 {
-   //  ENSURE THAT RECEIVED PARAMETERS ARE VALID
-    if (volumename == NULL || pathname == NULL||entrynames== NULL 
-        ||nentries== NULL ||modtime== NULL){
+    //  ENSURE THAT RECEIVED PARAMETERS ARE VALID
+    if (volumename == NULL || pathname == NULL || entrynames == NULL || nentries == NULL || modtime == NULL)
+    {
         SIFS_errno = SIFS_EINVAL;
         return 1;
     }
@@ -33,20 +32,37 @@ int SIFS_dirinfo(const char *volumename, const char *pathname,
     SIFS_BIT bitmap[volHeader.nblocks];
     fread(&bitmap, sizeof bitmap, 1, vol);
 
-//下面的内容是多层目录的支持，先把其他的功能写完之后再写这个
-// 已经把将要写的内容转移到 pathmatch 到时候直接调用就可
-//我们现在假设现在得到的 blockID 就是文件夹的目录
+    //下面的内容是多层目录的支持，先把其他的功能写完之后再写这个
+    // 已经把将要写的内容转移到 pathmatch 到时候直接调用就可
+    //我们现在假设现在得到的 blockID 就是文件夹的目录
     int blockID = SIFS_pathmatch(vol, pathname);
     SIFS_DIRBLOCK block;
-    fseek(vol,sizeof volHeader+sizeof bitmap+volHeader.blocksize*blockID,SEEK_SET);
+    fseek(vol, sizeof volHeader + sizeof bitmap + volHeader.blocksize * blockID, SEEK_SET);
     fread(&block, sizeof block, 1, vol);
-    // strcpy(***entrynames,block.name);
-    *nentries=block.nentries;
-    *modtime=block.modtime;
+    *nentries = block.nentries;
+    *modtime = block.modtime;
     for (int i = 0; i < block.nentries; i++)
     {
-        strcpy(*entrynames[i],"a");
+        int checkingBlockID = block.entries[i].blockID;
+        if (bitmap[checkingBlockID] == 'd')
+        {
+            SIFS_DIRBLOCK newBlock;
+            fseek(vol, sizeof volHeader + sizeof bitmap + volHeader.blocksize * checkingBlockID, SEEK_SET);
+            fread(&newBlock, sizeof newBlock, 1, vol);
+            strcpy(*entrynames[i], newBlock.name);
+        }
+        else if (bitmap[checkingBlockID] == 'f')
+        {
+            SIFS_FILEBLOCK newBlock;
+            fseek(vol, sizeof volHeader + sizeof bitmap + volHeader.blocksize * checkingBlockID, SEEK_SET);
+            fread(&newBlock, sizeof newBlock, 1, vol);
+            strcpy(*entrynames[i], newBlock.filenames[block.entries[i].fileindex]);
+        }
+
     }
-    
+        //  FINISHED, CLOSE THE VOLUME
+
+        fclose(vol);
+
     return 0;
 }
