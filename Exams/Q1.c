3fe5f4a1580a15e82d9318841548f4493715f424
Tommy <tommyzhangchina@gmail.com>
2019-10-26 00:08:37 +0800

    E 1.2

--- Exams/Q1.c
+++ Exams/Q1.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdbool.h>
-
+#include <stdio.h> //printf
+#include <stdlib.h> //atoi()
 /* 
 The purpose of the function is to determine 
 if the character string pointed to by needle 
@@ -39,5 +40,73 @@ bool subString(char *haystack, char *needle)
         string++; // CHECK FROM THE NEXT CHAR UNTIL THE END OF HAYSTACK
     }
     // IF NOT FOUND MATCH UNTIL THE END OF HAYSTACK, RETURN FALSE
-    return false; 
+    return false;
+}
+
+/*
+The function takes four integer parameters,
+each providing information about a dateand its year.
+
+The goal of the function is to firstly ensure that
+the provided date is valid and, if so, to determine
+on which day of the week the date falls. 
+
+The first parameter, day, provides the day of interest − its
+values must range from 1 to 31 (inclusive). 
+
+The second parameter, month, provides the month of interest
+− its values must range from 1 to 12 (inclusive). 
+
+The third parameter, year, provides the year of interest
+− any integer value of 1970 or greater is permitted. 
+
+The final parameter, firstJan, indicates the day
+of the week on which the first of January falls
+in the provided year. Its permitted values are 0 
+(representing Sunday), 1 (representing Monday), 
+and so on, up to 6 (representing Saturday).
+
+If the provided date is invalid, 
+the function should return the integer -1.
+Write the function dayOfWeek() in C99.
+*/
+int dayOfWeek(int day, int month, int year, int firstJan)
+{
+    int leap = 0; // IF THIS IS LEAP YEAR
+    int dayOfYear = 0;
+    if (day < 1 || day > 31 || month < 1 || month > 12 || year < 1970)
+        return -1;
+    //  CHECK IF THIS YEAR IS LEAP YEAR
+    if (year % 4 == 0 && year % 100 != 0)
+        leap = 1;
+    else if (year % 400 == 0)
+        leap = 1;
+
+    if (!leap && month == 2 && day > 28)
+        return -1;
+    for (int i = 1; i <= 12; i++)
+    {
+        if (i < month)
+        {
+            if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10)
+                dayOfYear += 31;
+            else if (i == 4 || i == 6 || i == 9 || i == 11)
+                dayOfYear += 30;
+            else if (leap)
+                dayOfYear += 29;
+            else
+                dayOfYear += 28;
+        }
+        else
+        {
+            dayOfYear += day;
+            return (dayOfYear - 1 + firstJan) % 7;
+        }
+    }
+    return -1;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("%i\n", dayOfWeek(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4])));
 }
