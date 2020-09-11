3fa038e8aeab72763cd65742517a47cde3ecd478
ChaoyuHan <71022032+ChaoyuHan@users.noreply.github.com>
2020-09-11 16:35:14 +0800

    2020 P1 DONE

--- 2020 P1/pipesim.c
+++ 2020 P1/pipesim.c
@@ -8,13 +8,6 @@
     Student number(s):   22384028
  */
 
-/* TO DO LIST
-1. Make a queue of process
-2. Store the event(system-call) of every process
-
-
-*/
-
 //  MAXIMUM NUMBER OF PROCESSES OUR SYSTEM SUPPORTS (PID=1..20)
 #define MAX_PROCESSES 20
 
@@ -34,24 +27,20 @@
 //  YOUR DATA STRUCTURES, VARIABLES, AND FUNCTIONS SHOULD BE ADDED HERE:
 
 //  THIS IS DATA STRUCTURE FOR ARGUMENT
-// char fileName[MAX_PROCESSES];                          // Argument1
-int TQ; //Argument2
-// int PipeSize; //Argument3
+int TQ; // Timequantum
 
 //  ---------------------------------------------------------------------
 
 // THIS IS DATA STRUCTURE FOR PROCESSES
-// int EventNum[MAX_PROCESSES];                                    // The total number of event of a process
-// int EventPID[MAX_PROCESSES][MAX_PIPE_DESCRIPTORS_PER_PROCESS];  // The PID of event
 int eventType[MAX_PROCESSES][MAX_SYSCALLS_PER_PROCESS];         // The type of event
 int eventTime[MAX_PROCESSES][MAX_PIPE_DESCRIPTORS_PER_PROCESS]; //The duation of event
 int eventWindow[MAX_PROCESSES];                                 // The window of the event
 int eventTail[MAX_PROCESSES];                                   // The tail of the event
 int exitEvent[MAX_PROCESSES];                                   // The process ID waiting for this child process
+int running = 1;                                                // To see if the process is runing
+int first = 1;                                                  // If the first execution
+int numProcess = 1;                                             // The number of total processes
 int timetaken = 0;                                              // Total time
-int running = 1;
-int first = 1;
-int numProcess = 1;
 
 //  ---------------------------------------------------------------------
 
@@ -154,6 +143,7 @@ void parse_eventfile(char program[], char eventfile[])
         //  THIS FUNCTION ONLY CHECKS INPUT;  YOU WILL NEED TO STORE THE VALUES
         if (nwords == 3 && strcmp(words[1], "compute") == 0)
         {
+            // Record the time to compute
             usecs = check_microseconds(words[2], lc);
             eventType[thisPID][eventTail[thisPID]] = 0;
             eventTime[thisPID][eventTail[thisPID]] = atoi(words[2]);
@@ -162,6 +152,7 @@ void parse_eventfile(char program[], char eventfile[])
 
         else if (nwords == 3 && strcmp(words[1], "sleep") == 0)
         {
+            // Record the time to sleep
             usecs = check_microseconds(words[2], lc);
             eventType[thisPID][eventTail[thisPID]] = 1;
             eventTime[thisPID][eventTail[thisPID]] = atoi(words[2]);
@@ -171,12 +162,12 @@ void parse_eventfile(char program[], char eventfile[])
         else if (nwords == 2 && strcmp(words[1], "exit") == 0)
         {
             eventType[thisPID][eventTail[thisPID]] = 2;
-            // eventTime[thisPID][eventTail[thisPID]] = atoi(words[2]);
             eventTail[thisPID]++;
         }
 
         else if (nwords == 3 && strcmp(words[1], "fork") == 0)
         {
+            // Record the child process ID
             otherPID = check_PID(words[2], lc);
             eventType[thisPID][eventTail[thisPID]] = 3;
             eventTime[thisPID][eventTail[thisPID]] = otherPID;
@@ -185,6 +176,7 @@ void parse_eventfile(char program[], char eventfile[])
 
         else if (nwords == 3 && strcmp(words[1], "wait") == 0)
         {
+            // Record the child process ID to wait
             otherPID = check_PID(words[2], lc);
             eventType[thisPID][eventTail[thisPID]] = 4;
             exitEvent[otherPID] = thisPID;
@@ -193,28 +185,13 @@ void parse_eventfile(char program[], char eventfile[])
 
         else if (nwords == 3 && strcmp(words[1], "pipe") == 0)
         {
-            // pipedesc = check_descriptor(words[2], lc);
-            // eventType[thisPID][eventTail[thisPID]] = 5;
-            // eventTime[thisPID][eventTail[thisPID]] = atoi(words[2]);
-            // eventTail[thisPID] ++;
         }
         else if (nwords == 4 && strcmp(words[1], "writepipe") == 0)
         {
-            // pipedesc = check_descriptor(words[2], lc);
-            // nbytes = check_bytes(words[3], lc);
-            // eventType[thisPID][eventTail[thisPID]] = 6;
-            // eventTime[thisPID][eventTail[thisPID]] = atoi(words[2]);
-            // eventTail[thisPID] ++;
         }
         else if (nwords == 4 && strcmp(words[1], "readpipe") == 0)
         {
-            // pipedesc = check_descriptor(words[2], lc);
-            // nbytes = check_bytes(words[3], lc);
-            // eventType[thisPID][eventTail[thisPID]] = 7;
-            // eventTime[thisPID][eventTail[thisPID]] = atoi(words[2]);
-            // eventTail[thisPID] ++;
         }
-        //  UNRECOGNISED LINE
         else
         {
             printf("%s: line %i of '%s' is unrecognized\n", program, lc, eventfile);
@@ -229,18 +206,21 @@ void parse_eventfile(char program[], char eventfile[])
 }
 
 //  ---------------------------------------------------------------------
-int queue[MAX_PROCESSES]; //
-int window = 0;
-int tail = 0;
+int queue[MAX_PROCESSES]; // The Queue of ready process
+int window = 0;           // The head of the queue
+int tail = 0;             // The end of the queue
 
 int readQ()
 {
+    // Check if the queue is empty
     if (window >= tail || (tail == 49 && window == 0))
     {
         return -1;
     }
 
     int result = queue[window];
+
+    // Move the head to the next position
     if (window == 49)
     {
         window = 0;
@@ -254,6 +234,7 @@ int readQ()
 void writeQ(int pid)
 {
     queue[tail] = pid;
+    // Move the tail to the next position
     if (tail == 49)
     {
         tail = 0;
@@ -264,31 +245,74 @@ void writeQ(int pid)
     }
 }
 //  ---------------------------------------------------------------------
+int sleepQueue[MAX_PROCESSES]; // The sleep staus of each process
+int sleepTime[MAX_PROCESSES];  // The time of awake of each process
+void checkSleeping()
+{
+    // Check if any process wakes in current time
+    for (int i = 1; i < MAX_PROCESSES; i++)
+    {
+        if (sleepQueue[i] && sleepTime[i] <= timetaken)
+        {
+            sleepQueue[i] = 0;
+            writeQ(i);
+        }
+    }
+}
+//  ---------------------------------------------------------------------
+// The main simulation of process executing
 void execute()
 {
-    int currentProcess;
+    int currentProcess; // The PID of current running process
+
+    // Check if this is the first time to execute
     if (first)
     {
+        // Set running process to 1
         currentProcess = 1;
         first = 0;
     }
     else
     {
+        // Get a process from the queue
         currentProcess = readQ();
+        // Check if the queue empty
         if (currentProcess == -1)
         {
-            running = 0;
+            // Check if anyone sleeping
+            int recentAwake = 999999;
+            // Find out the recent awake time
+            for (int i = 1; i < MAX_PROCESSES; i++)
+            {
+                if (sleepQueue[i] && sleepTime[i] < recentAwake)
+                {
+                    recentAwake = sleepTime[i];
+                }
+            }
+            // If no process in the queue, skip time to the nearest awake time
+            if (recentAwake < 999999)
+            {
+                timetaken = recentAwake;
+                checkSleeping();
+            }
+            else
+            {
+                // No process sleeping and waiting, finalise the execution
+                running = 0;
+            }
             return;
         }
+        // Change the state of Ready to Running
         timetaken += 5;
     }
 
-    int win = eventWindow[currentProcess];
-    int childPid;
+    int win = eventWindow[currentProcess]; // The head of event queue of current running process
+
+    int childPid; // Child PID for fork() and wait()
     switch (eventType[currentProcess][win])
     {
-    case 0:
-        // timetaken += 5;
+        // Check the event(System call) type of the event
+    case 0: // Compute
         if (eventTime[currentProcess][win] <= TQ)
         {
             timetaken += eventTime[currentProcess][win];
@@ -301,35 +325,41 @@ void execute()
         }
         writeQ(currentProcess);
         timetaken += 5;
-
         break;
-    case 1:
-        /* code */
+    case 1: // Sleep
+        timetaken += 10;
+        // Put current process into the sleep queue
+        sleepQueue[currentProcess] = 1;
+        sleepTime[currentProcess] = timetaken + eventTime[currentProcess][win];
+        eventWindow[currentProcess]++;
         break;
-    case 2:
+    case 2: // EXIT
         timetaken += 5;
-        // Check if any process waiting
+        // Check if any process waiting this child process exit
         if (exitEvent[currentProcess] != 0)
         {
             writeQ(exitEvent[currentProcess]);
         }
 
         break;
-    case 3:
+    case 3: // Fork
         childPid = ++numProcess;
+        // Add the child process into the queue
         writeQ(childPid);
         writeQ(currentProcess);
         timetaken += 10;
         eventWindow[currentProcess]++;
 
         break;
-    case 4:
+    case 4: // Wait
         timetaken += 10;
+        // Doing nothing, and will be actived when the waiting child process exit
         eventWindow[currentProcess]++;
         break;
     default:
         break;
     }
+    checkSleeping(); // Check any process awake upon the complection of current event
 }
 
 //  ---------------------------------------------------------------------
@@ -343,6 +373,7 @@ int main(int argcount, char *argvalue[])
     fileName = argvalue[1];
     TQ = atoi(argvalue[2]);
 
+    // Initialize the array variables
     for (size_t i = 0; i < MAX_PROCESSES; i++)
     {
         exitEvent[i] = 0;
@@ -350,17 +381,15 @@ int main(int argcount, char *argvalue[])
 
     // Parse File
     parse_eventfile(argvalue[0], fileName);
-
     timetaken = 0;
 
-    // Simulate
-    // queue[0] = 1;
-    // writeQ(1);
+    // Start Simulation
     while (running)
     {
         execute();
     }
 
+    // Return result
     printf("timetaken %i\n", timetaken);
     return 0;
 }
